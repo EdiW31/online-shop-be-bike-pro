ecaa16071824c084c7e378b049e5d3b78a138306
EdiW31 <129823940+EdiW31@users.noreply.github.com>
2024-07-05 12:57:01 +0300

    Update favorites.controller.ts

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { CreateFavoriteDto } from './dto/create-favorite.dto';
-import { UpdateFavoriteDto } from './dto/update-favorite.dto';
 
 @Controller('favorites')
 export class FavoritesController {
